9c3e8842b8b9395b7c50cabf72a4a48fea52c397
Małgorzata Wasilewska <wu.a11@outlook.com>
2023-05-07 16:25:03 +0200

    Zadanie dzien 4

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+var age = 33;
+string sex = "Kobieta";
+
+if (sex == "Kobieta" || age < 30)
+{
+    Console.WriteLine("Kobieta ponożej 30 lat");
+}
+else if (name == "Ewa"&& age ==33 )
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != "Kobieta" && age < 33 )
+{
+    Console.WriteLine(" Niepełnoletni mężczyzna");
+}
+
+
+
+
+
